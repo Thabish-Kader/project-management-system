21c2059a17b87b73fa00d79f1f8cf510bfd07d62
Thabish <kadertabish2@gmail.com>
2023-01-13 15:48:52 +0400

    query-created
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- server/src/schema/schema.ts
+++ server/src/schema/schema.ts
@@ -1,10 +1,12 @@
 import {
 	GraphQLID,
+	GraphQLList,
 	GraphQLObjectType,
 	GraphQLSchema,
 	GraphQLString,
 } from "graphql";
 import ClientModel from "../models/Client";
+import ProjectModel from "../models/Project";
 
 const ClientType = new GraphQLObjectType({
 	name: "Client",
@@ -16,6 +18,22 @@ const ClientType = new GraphQLObjectType({
 	},
 });
 
+const ProjectType = new GraphQLObjectType({
+	name: "Client",
+	fields: {
+		id: { type: GraphQLID },
+		name: { type: GraphQLString },
+		description: { type: GraphQLString },
+		status: { type: GraphQLString },
+		client: {
+			type: ClientType,
+			resolve(parent, args) {
+				return ClientModel.findById(parent.clientId);
+			},
+		},
+	},
+});
+
 const RootQuery = new GraphQLObjectType({
 	name: "RootQueryType",
 	fields: {
@@ -24,6 +42,25 @@ const RootQuery = new GraphQLObjectType({
 			args: { id: { type: GraphQLID } },
 			resolve(parent, args) {
 				return ClientModel.findById(args.id);
+			},
+		},
+		clients: {
+			type: new GraphQLList(ClientType),
+			resolve(parent, args) {
+				return ClientModel.find();
+			},
+		},
+		project: {
+			type: ProjectType,
+			args: { id: { type: GraphQLID } },
+			resolve(parent, args) {
+				return ProjectModel.findById(args.id);
+			},
+		},
+		projects: {
+			type: new GraphQLList(ProjectType),
+			resolve(parent, args) {
+				return ProjectModel.find();
 			},
 		},
 	},
