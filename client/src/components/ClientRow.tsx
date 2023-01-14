4ec6780730c259aab081a2022934d634bdbb964e
Thabish <kadertabish2@gmail.com>
2023-01-14 16:35:13 +0400

    spinner-added
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- client/src/components/ClientRow.tsx
+++ client/src/components/ClientRow.tsx
@@ -1,7 +1,22 @@
+import { FaTrash } from "react-icons/fa";
 type Props = {
 	client: TClient;
 };
 
 export const ClientRow = ({ client }: Props) => {
-	return <div>ClientRow</div>;
+	return (
+		<tr>
+			<td>{client.name}</td>
+			<td>{client.email}</td>
+			<td>{client.phone}</td>
+			<td>
+				<button
+					// onClick={() => deleteClient()}
+					className="btn btn-danger btn-sm"
+				>
+					<FaTrash />
+				</button>
+			</td>
+		</tr>
+	);
 };
--- client/src/components/Clients.tsx
+++ client/src/components/Clients.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@apollo/client";
 import { GET_CLIENTS } from "../queries/clientQueries";
 import { ClientRow } from "./ClientRow";
+import { Spinner } from "./Spinner";
 
 type TClients = {
 	clients: TClient[];
@@ -9,20 +10,27 @@ type TClients = {
 export const Clients = () => {
 	const { loading, error, data } = useQuery<TClients>(GET_CLIENTS);
 
+	if (loading) return <Spinner />;
+	if (error) return <p>Something went wrong</p>;
+
 	return (
-		<table className="table table-secondary bg-slate">
-			<thead>
-				<tr>
-					<th>Name</th>
-					<th>Email</th>
-					<th>Phone</th>
-				</tr>
-			</thead>
-			<tbody>
-				{data?.clients.map((client) => (
-					<ClientRow key={client.id} client={client} />
-				))}
-			</tbody>
-		</table>
+		<>
+			{!loading && !error && (
+				<table className="table table-hover mt-3">
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Email</th>
+							<th>Phone</th>
+						</tr>
+					</thead>
+					<tbody>
+						{data?.clients.map((client) => (
+							<ClientRow key={client.id} client={client} />
+						))}
+					</tbody>
+				</table>
+			)}
+		</>
 	);
 };
