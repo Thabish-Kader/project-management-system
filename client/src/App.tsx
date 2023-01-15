bc1699141277ad24ba7ad5b6240b63741a68705e
Thabish <kadertabish2@gmail.com>
2023-01-15 16:22:03 +0400

    home-layout-added
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,14 @@
-import { useState } from "react";
 import { Navbar } from "./components/Navbar";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import { Clients } from "./components/Clients";
-import { AddClientModal } from "./components/AddClientModal";
-import { Projects } from "./components/Projects";
 
-const client = new ApolloClient({
-	uri: "http://localhost:5001/graphql",
-	cache: new InMemoryCache(),
-});
+import { Home } from "./pages/Home";
 
 function App() {
 	return (
 		<>
-			<ApolloProvider client={client}>
-				<Navbar />
-				<div className="container">
-					<AddClientModal />
-					<Projects />
-					<Clients />
-				</div>
-			</ApolloProvider>
+			<Navbar />
+			<div className="container">
+				<Home />
+			</div>
 		</>
 	);
 }
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { AddClientModal } from "../components/AddClientModal";
+import { Projects } from "../components/Projects";
+import { Clients } from "../components/Clients";
+import { AddProjectModal } from "../components/AddProjectModal";
 
 export const Home = () => {
-	return <div>Home</div>;
+	return (
+		<>
+			<div className="d-flex gap-3 mb-4">
+				<AddClientModal />
+				<AddProjectModal />
+			</div>
+			<Projects />
+			<hr />
+			<Clients />
+		</>
+	);
 };
